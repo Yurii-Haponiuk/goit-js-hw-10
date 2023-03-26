bfceb82c1798724423d7ef30c134b973abf9d9d7
Yurii-Haponiuk <yurii.gaponyuk@gmail.com>
2023-03-26 14:55:26 +0300

    searchParams start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const searchBox = document.querySelector('.#search-box');
+const countriesList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+searchBox.addEventListener('click', e => {
+    e.preventDefault();
+})
