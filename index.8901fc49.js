bd800a2adf055e3d117c4c01a608025bee377e62
Yurii-Haponiuk <113940294+Yurii-Haponiuk@users.noreply.github.com>
2023-03-27 05:26:54 +0000

    Deploying to gh-pages from @ Yurii-Haponiuk/goit-js-hw-10@9635a8cc84282e7a25beb0a89757974b3b59dcb9 🚀

--- index.8901fc49.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".#search-box");document.querySelector(".country-list"),document.querySelector(".country-info");e.addEventListener("click",(function(e){e.preventDefault()}))}();
-//# sourceMappingURL=index.8901fc49.js.map
